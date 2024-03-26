dfe57d0547a785e2baf6a45aaac3c8b3b54992a5
naveenkrishnan840 <58537261+naveenkrishnan840@users.noreply.github.com>
2024-03-26 11:38:33 +0530

    Update views.py
    request status format changed

--- social_media_backend/social_media_project/social_media_app/views.py
+++ social_media_backend/social_media_project/social_media_app/views.py
@@ -21,7 +21,7 @@ def send_request_check(request):
     from_user = request.data[1]["from_user"]
     to_user = request.data[1]["to_user"]
     if models.FriendRequestStatus.objects.filter(from_user_id=from_user, to_user_id=to_user,
-                                                 request_status__in=[0, 1]).exists():
+                                                 request_status__in=[1, 2]).exists():
         response = ["SendRequestFailed", {}]
     else:
         models.FriendRequestStatus.objects.create(from_user_id=from_user, to_user_id=to_user)
@@ -67,7 +67,7 @@ class SocialMedia(APIView):
             user_details = serializer.UserRegistration(other_user, many=True).data
             for i in user_details:
                 if i["user_id"] in request_users:
-                    i["status"] = "1,2"
+                    i["status"] = "2,3"
                 else:
                     i["status"] = "0"
             response = ["GetAllUsersSuccess", user_details]
@@ -76,7 +76,7 @@ class SocialMedia(APIView):
             get_user_id = request.data[1]["user_id"]
             pending_users = (models.FriendRequestStatus.objects.annotate(to_user_name=F("to_user__user_name"),
                                                                          to_email_id=F("to_user__email_id")).
-                             filter(from_user_id=get_user_id, request_status=0).values("to_user_name", "to_user_id",
+                             filter(from_user_id=get_user_id, request_status=1).values("to_user_name", "to_user_id",
                                                                                        "to_email_id"))
             pending_users = serializer.FriendRequestStatus(pending_users, many=True).data
             response = ["GetPendingUsersSuccess", pending_users]
@@ -85,7 +85,7 @@ class SocialMedia(APIView):
             get_user_id = request.data[1]["user_id"]
             accept_users = (models.FriendRequestStatus.objects.annotate(to_user_name=F("to_user__user_name"),
                                                                         to_email_id=F("to_user__email_id")).
-                            filter(from_user_id=get_user_id, request_status__in=[1, 2]).values("to_user_name",
+                            filter(from_user_id=get_user_id, request_status__in=[2, 3]).values("to_user_name",
                                                                                                "to_user_id",
                                                                                                "to_email_id"))
             accept_users = serializer.FriendRequestStatus(accept_users, many=True).data
@@ -94,7 +94,7 @@ class SocialMedia(APIView):
         elif request.data[0] == "ChangeStatus":
             from_user = request.data[1]["from_user"]
             to_user = request.data[1]["to_user"]
-            status = 1 if request.data[1]["status"] == "accept" else 2
+            status = 2 if request.data[1]["status"] == "accept" else 3
             (models.FriendRequestStatus.objects.filter(from_user_id=from_user, to_user_id=to_user).
              update(request_status=status))
             response = ["StatusChanged", {}]
