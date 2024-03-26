1fb0688163c23819be675281bd3013c68ff8cc23
naveenkrishnan840 <58537261+naveenkrishnan840@users.noreply.github.com>
2024-03-26 20:00:24 +0530

    Update models.py

--- social_media_backend/social_media_project/social_media_app/models.py
+++ social_media_backend/social_media_project/social_media_app/models.py
@@ -8,15 +8,12 @@ class UserRegistration(models.Model):
     user_name = models.CharField(max_length=100)
     email_id = models.CharField(max_length=100)
     password = models.CharField(max_length=1000)
-    request_complete = models.IntegerField(default="0")
 
     class Meta:
         db_table = "tbl_users"
 
 
 class FriendRequestStatus(models.Model):
-    # from_user = models.ForeignKey(to=UserRegistration, on_delete=models.CASCADE)
-    # to_user = models.ForeignKey(to=UserRegistration, on_delete=models.CASCADE)
     from_user_id = models.IntegerField()
     to_user = models.ForeignKey(to=UserRegistration, on_delete=models.CASCADE)
     request_status = models.IntegerField(default="0")
