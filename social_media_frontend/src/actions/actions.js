0a6abcf506d6383ee5cce98105f1845618b74601
naveenkrishnan840 <58537261+naveenkrishnan840@users.noreply.github.com>
2024-03-26 11:05:59 +0530

    Update actions.js

--- social_media_frontend/src/actions/actions.js
+++ social_media_frontend/src/actions/actions.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import {message} from "antd";
-import {useHistory} from "react-router-dom"
 axios.defaults.baseURL = "http://127.0.0.1:8081/";
 export const register = ({payload, navigate}) => async dispatch=>  {
     try {
